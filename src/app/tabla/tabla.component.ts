decec819c088bad8344fcf99ae8c1f80917be529
Emiliano Daián Hereñu <emilianodaian@gmail.com>
2023-09-21 15:46:42 -0300

    tabla con datos

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
   styleUrls: ['./tabla.component.css']
 })
-export class TablaComponent {
-
+export class TablaComponent  {
+  item=[
+    {id:1,nombre:'ramiro',email:'ramiro@gmail.com'},
+    {id:2,nombre:'luca',email:'luca@gmail.com'},
+    {id:3,nombre:'daian',email:'daian@gmail.com'}
+  ];
+  constructor(){
+  }
+  ngOnInit(){
+  }
 }
