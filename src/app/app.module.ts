a88bb6d4a9b56de5a253f575c2bed2e9d9767277
Emiliano Daián Hereñu <emilianodaian@gmail.com>
2023-09-21 14:47:00 -0300

    creo componente tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import {FormsModule} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule,
